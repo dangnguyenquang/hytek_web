85f845afbf59dc30cba37ab83e3684d80b33057a
dangnguyenquang <dangnguyenquangit@gmail.com>
2023-10-10 08:56:10 +0700

    fix bug API show

--- code/BE/controllers/ShowControllers.js
+++ code/BE/controllers/ShowControllers.js
@@ -2,35 +2,39 @@ const productInfo = require("../models/productInfo");
 
 class showControllers {
     show (req, res) {
-        var result = 0;
-        var message = '';
-        var total_data = 0;
-        var resjson = {};
-
-        productInfo.find({}, (err, data) => {
-            if (err) {
-              console.error('Lỗi khi lấy dữ liệu:', err);
-              result = 0;
-              message = `Lỗi khi lấy dữ liệu: ${err}`;
-              total_data = 0;
-              data = [];
-            } else {
-              console.log(' Lấy dữ liệu thành công từ collection productInfo');
-              result = 1;
-              message = `Thành công`;
-              total_data = data.length;
-            }
-            resjson = {
-                result: result,
-                message: message,
-                total_data: total_data,
-                data: data,
-            }
-        });
+      productInfo.find({})
+      .then((data) => {
+        console.log('Lấy dữ liệu thành công từ collection productInfo');
+        const result = 1;
+        const message = 'Thành công';
+        const total_data = data.length;
         
+        const resjson = {
+          result: result,
+          message: message,
+          total_data: total_data,
+          data: data,
+        };
+    
         res.json(resjson);
+      })
+      .catch((err) => {
+        console.error('Lỗi khi lấy dữ liệu:', err);
+        const result = 0;
+        const message = `Lỗi khi lấy dữ liệu: ${err}`;
+        const total_data = 0;
+        const data = [];
+    
+        const resjson = {
+          result: result,
+          message: message,
+          total_data: total_data,
+          data: data,
+        };
+    
+        res.json(resjson);
+      });
     }
-
 }
 
 module.exports = new showControllers();
